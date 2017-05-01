dacbb2ea0b9090a4783253259dced055da05ef26
Teun Zwart <teunzwart@gmail.com>
2017-05-02 00:18:39 +0200

    Refactor journal abbreviations into seperate file

---
+++ journal_abbreviations.py
@@ -0,0 +1,13 @@
+"""Some additional journal abbreviations which are not provided by Crossref."""
+
+JOURNAL_ABBRVS = {
+    "American Journal of Mathematics": "Am. J. Math.",
+    "American Journal of Physics": "Am. J. Phys",
+    "Annals of Physics": "Ann. Phys.",
+    "Communications in Mathematical Physics": "Commun. Math. Phys.",
+    "Journal of Mathematical Physics": "J. Math. Phys.",
+    "Journal of Statistical Physics": "J. Stat. Phys.",
+    "Nature Physics": "Nat. Phys.",
+    "Nuclear Physics B": "Nucl. Phys. B",
+    "Physics Letters A": "Phys. Lett. A"
+}
