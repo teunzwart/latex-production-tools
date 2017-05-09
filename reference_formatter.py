ec15a485d63d4e44b696f9830424d019ab52358f
Teun Zwart <teunzwart@gmail.com>
2017-05-09 22:43:52 +0200

    Parallelize reference formatting

--- reference_formatter.py
+++ reference_formatter.py
@@ -1,18 +1,29 @@
 """Automatically format references in a LaTeX file."""
 
 import argparse
+from multiprocessing import Pool
 
 from reference_utils import Reference, extract_bibtex_items
 from latex_utils import read_latex_file, write_latex_file
 
 
+def get_reference(bibtex_entry):
+    """Wrapper for multithreading."""
+    reference = Reference(bibtex_entry.rstrip())
+    reference.main()
+    return reference.bibitem_data, reference.bibitem_identifier, reference.reformatted_original_reference, reference.formatted_reference
+
+
 def format_references(latex_source):
+    """Format all references in the given LaTeX source."""
     bibtex_entries = extract_bibtex_items(latex_source)
-    for i, bibtex_entry in enumerate(bibtex_entries):
-        print(f"Processing reference {i+1:>{len(str(len(bibtex_entries)))}} of {len(bibtex_entries)}...")
-        reference = Reference(bibtex_entry.rstrip())
-        reference.main()
-        latex_source = latex_source.replace(reference.bibitem_data, f"\\bibitem{{{reference.bibitem_identifier}}} TODO\n{reference.reformatted_original_reference}\n\n%{reference.formatted_reference}\n\n\n")
+    # Parallelising the reference lookup gives a 15x speedup.
+    # Values larger than 15 for the poolsize do not give a further speedup.
+    with Pool(15) as pool:
+        res = pool.map(get_reference, bibtex_entries)
+    for r in res:
+        bibitem_data, bibitem_identifier, reformatted_original_reference, formatted_reference = r
+        latex_source = latex_source.replace(bibitem_data, f"\\bibitem{{{bibitem_identifier}}} TODO\n{reformatted_original_reference}\n\n%{formatted_reference}\n\n\n")
     return latex_source
 
 
@@ -21,5 +32,6 @@ if __name__ == '__main__':
     parser.add_argument('latex_file')
     args = parser.parse_args()
     latex_source = read_latex_file(args.latex_file)
+    print("Processing references...")
     latex_source = format_references(latex_source)
     write_latex_file(args.latex_file, latex_source)
